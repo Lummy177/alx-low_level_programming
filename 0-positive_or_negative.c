96c32f41557ce7b2d8f3dcb53ec825adf7139a52
Lummy177 <Pelumiajayi04@gmail.com>
2023-08-13 09:22:04 -0700

    Positive

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 
+
 /**
  * main - Prints if number is positive, zero or 2 negative
  * Return: Always (Success)
@@ -15,8 +16,10 @@ int main(void)
 {
 	
 	int n;
-        srand(time(0));
+        
+	srand(time(0));
         n = rand() - RAND_MAX / 2;
+
         if (n > 0)
         {
         printf("%d is positive\n", n);
