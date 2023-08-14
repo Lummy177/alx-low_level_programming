4572688c596299bc78964609955033798f2d75df
Lummy177 <Pelumiajayi04@gmail.com>
2023-08-14 06:23:45 -0700

    Helloo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /** 
  * main - A program that prints a line with put function
+ *
  * Return : 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
